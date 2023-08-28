757b3462282b3319d831f2100aa180f47c329142
Vu Dang <dtvu94@gmail.com>
2023-08-28 21:49:53 +0700

    Update template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('myEngineer');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myEngineer app is running!');
-  });
 });
